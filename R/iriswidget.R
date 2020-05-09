1577a8db279a4fa76a9abc1efb6be51a57b4d944
root <samrickman+github@gmail.com>
2020-05-09 10:32:40 +0000

    flickering widget hardcoded

--- R/iriswidget.R
+++ R/iriswidget.R
@@ -6,31 +6,21 @@
 #'
 #' @export
 iriswidget <- function(message, width = NULL, height = NULL, elementId = NULL) {
-    # print(readbitmap::read.bitmap(("iris.png")))
-  # forward options using x
-  x = list(
-    message = message,
 
-    # This doesn't work for the data -
-    # basically it seems almost impossible to
-    # pass an image to JS from R - or at least I
-    # can't work out how. Also there is the
-    # secondary question of whether it's a good idea.
-    # You will ultimately end up working backwards from
-    # pixels. Might it not be a better idea to pass data?
+    # forward options using x
+    x = list(
+        message = message
+    )
 
-    data = readBin("iris.png", what="raw")
-  )
-
-  # create widget
-  htmlwidgets::createWidget(
-    name = 'iriswidget',
-    x,
-    width = width,
-    height = height,
-    package = 'iriswidget',
-    elementId = elementId
-  )
+    # create widget
+    htmlwidgets::createWidget(
+        name = 'iriswidget',
+        x,
+        width = width,
+        height = height,
+        package = 'iriswidget',
+        elementId = elementId
+    )
 }
 
 
@@ -53,12 +43,12 @@ iriswidget <- function(message, width = NULL, height = NULL, elementId = NULL) {
 #'
 #' @export
 iriswidgetOutput <- function(outputId, width = '100%', height = '400px'){
-  htmlwidgets::shinyWidgetOutput(outputId, 'iriswidget', width, height, package = 'iriswidget')
+    htmlwidgets::shinyWidgetOutput(outputId, 'iriswidget', width, height, package = 'iriswidget')
 }
 
 #' @rdname iriswidget-shiny
 #' @export
 renderIriswidget <- function(expr, env = parent.frame(), quoted = FALSE) {
-  if (!quoted) { expr <- substitute(expr) } # force quoted
-  htmlwidgets::shinyRenderWidget(expr, iriswidgetOutput, env, quoted = TRUE)
+    if (!quoted) { expr <- substitute(expr) } # force quoted
+    htmlwidgets::shinyRenderWidget(expr, iriswidgetOutput, env, quoted = TRUE)
 }
